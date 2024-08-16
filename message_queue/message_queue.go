735a091d7703be411c4ca8c184df5e3c9f40f2de
Jonathan Andrade Valenzuela <jmv1006@gmail.com>
2024-08-16 00:00:03 -0700

    base functionality with mutexes added

--- message_queue/message_queue.go
+++ message_queue/message_queue.go
@@ -1,11 +1,9 @@
 package mesage_queue
 
 import (
-	"bytes"
+	"bufio"
 	"encoding/base64"
 	"encoding/json"
-	"fmt"
-	"io"
 	"log"
 	"net"
 	"sync"
@@ -25,6 +23,7 @@ type MessageQueueConfig struct {
 type MessageQueue struct {
 	cfg      MessageQueueConfig
 	channels map[string]chan Message
+	mu       sync.Mutex
 }
 
 type Message struct {
@@ -44,8 +43,17 @@ func New(config MessageQueueConfig) *MessageQueue {
 func (mq *MessageQueue) Start() {
 	defer mq.cfg.Wg.Done()
 
+	// Locking mutex to start
+	mq.mu.Lock()
+
+	addr, err := net.ResolveTCPAddr(mq.cfg.Protocol, mq.cfg.Address)
+
+	if err != nil {
+		log.Println(err)
+		return
+	}
 	// Starting TCP listener
-	ln, err := net.Listen(mq.cfg.Protocol, mq.cfg.Address)
+	ln, err := net.ListenTCP(mq.cfg.Protocol, addr)
 
 	if err != nil {
 		log.Println(err)
@@ -53,48 +61,111 @@ func (mq *MessageQueue) Start() {
 	}
 
 	for {
-		conn, err := ln.Accept()
+		conn, err := ln.AcceptTCP()
 
 		if err != nil {
 			log.Println(err)
 			continue
 		}
 
-		var buf bytes.Buffer
-		_, err = io.Copy(&buf, conn)
+		// handle this connection on another threat
+		go mq.handleConnection(conn)
+	}
+
+}
+
+func (mq *MessageQueue) handleConnection(connPtr *net.TCPConn) {
+	// Read from connection
+	scanner := bufio.NewScanner(connPtr)
+
+	for scanner.Scan() {
+		req := Decode(scanner.Bytes())
+
+		if req == nil {
+			continue
+		}
+
+		if req.Type == "PRODUCE" {
+			mq.ProduceMessage(req, connPtr)
+		} else if req.Type == "CONSUME" {
+			// This will become a consuming connection
+			mq.CreateConsumerStream(connPtr)
+		}
+	}
+
+}
+
+func (mq *MessageQueue) CreateConsumerStream(connPtr *net.TCPConn) {
+	conn := *connPtr
+
+	// Create a new channel
+	consumerChan := make(chan Message)
+	id := uuid.New()
+
+	// Updating channels map
+	mq.mu.Unlock()
+	mq.channels[id.String()] = consumerChan
+	mq.mu.Lock()
+
+	// metrics
+	mq.cfg.MetricsHandler.AddChannel()
+
+	defer func() {
+		// Closing Connection
+		err := conn.Close()
+		if err != nil {
+			log.Printf("%s", err)
+			return
+		}
+		// Remove channel from metrics count
+		mq.cfg.MetricsHandler.RemoveChannel()
+
+		// Deleting channel from map
+		mq.mu.Unlock()
+		delete(mq.channels, id.String())
+		mq.mu.Lock()
+	}()
+
+	log.Printf("consumer %s connected to server", conn.LocalAddr())
+
+	// Heartbeat checker
+	go mq.checkConnectionHeartbeat(connPtr, consumerChan)
+
+	// Listen for queue updates - does not stop until the channel is closed
+	for msg := range consumerChan {
+		// marshal message
+		jsonMsg, _ := json.Marshal(msg)
+
+		resultMsg := make([]byte, 1000)
+
+		base64.StdEncoding.Encode(resultMsg, jsonMsg)
+
+		_, err := conn.Write(resultMsg)
 
 		if err != nil {
+			log.Printf("error producing message: %s", err)
+			return
+		}
+	}
+
+}
+
+func (mq *MessageQueue) checkConnectionHeartbeat(conn *net.TCPConn, channel chan Message) {
+	for {
+		one := make([]byte, 1)
+		_, err := conn.Write(one)
+
+		if err != nil {
+			// Write failed, connection might be dead
+			close(channel)
 			return
 		}
 
-		mq.handleMessage(buf, &conn)
+		time.Sleep(time.Second * 2)
 	}
-
 }
 
-func (mq *MessageQueue) handleMessage(msg bytes.Buffer, connPtr *net.Conn) {
-	conn := *connPtr
-	standardReq := Decode(msg.Bytes())
-
-	if standardReq == nil {
-		log.Println("message not valid")
-		conn.Close()
-		return
-	}
-
-	reqType := standardReq.Type
-
-	if reqType == "PRODUCE" {
-		// produce message to queue
-		mq.ProduceMessage(standardReq, connPtr)
-	} else if reqType == "CONSUME" {
-		// establish consumer connection, using go routine to not block other processes
-		go mq.CreateConsumerStream(connPtr)
-	}
-
-}
-
-func (mq *MessageQueue) ProduceMessage(msg *StandardRequest, connPtr *net.Conn) {
+func (mq *MessageQueue) ProduceMessage(msg *StandardRequest, connPtr *net.TCPConn) {
 	body := msg.Body
 	conn := *connPtr
 
@@ -122,69 +193,8 @@ func (mq *MessageQueue) ProduceMessage(msg *StandardRequest, connPtr *net.Conn) 
 	}
 }
 
-func (mq *MessageQueue) CreateConsumerStream(connPtr *net.Conn) {
-	conn := *connPtr
-
-	// Create a new channel
-	consumerChan := make(chan Message)
-	id := uuid.New()
-
-	mq.channels[id.String()] = consumerChan
-	// metrics
-	mq.cfg.MetricsHandler.AddChannel()
-
-	defer func() {
-		conn.Close()
-		fmt.Println("EXITING")
-		mq.cfg.MetricsHandler.RemoveChannel()
-		delete(mq.channels, id.String())
-
-	}()
-
-	log.Printf("consumer %s connected to server", conn.LocalAddr())
-
-	// Listen for queue updates
-	for msg := range consumerChan {
-		// marshal message
-		jsonMsg, _ := json.Marshal(msg)
-
-		resultMsg := make([]byte, 1000)
-
-		base64.StdEncoding.Encode(resultMsg, jsonMsg)
-
-		_, err := conn.Write(resultMsg)
-
-		if err != nil {
-			log.Printf("error producing message: %s", err)
-			return
-		}
-	}
-
-}
-
-func (mq *MessageQueue) handleChannelClosure(ch chan Message, connPtr *net.Conn) {
-	// Check if connection is alive every 3 seconds
-	conn := *connPtr
-
-	for {
-		_, err := conn.Read(make([]byte, 1))
-
-		if err != nil {
-			if err == io.EOF {
-				continue // Connection was closed by the peer
-			}
-			if netErr, ok := err.(net.Error); ok && netErr.Timeout() {
-				continue // Connection is still open, but timed out
-			}
-
-			close(ch)
-			return
-		}
-	}
-
-}
-
 func (mq *MessageQueue) NotifyConsumers(msg Message) {
+
 	for id, channel := range mq.channels {
 		log.Printf("notifying channel %s of message", id)
 		channel <- msg
